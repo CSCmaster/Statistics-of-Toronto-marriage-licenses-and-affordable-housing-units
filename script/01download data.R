d0208a8ff929d1f70cec1cbc1fb91b94dc3a69e8
Jingyi Shen <131424190+CSCmaster@users.noreply.github.com>
2024-01-24 17:02:52 -0500

    Add files via upload

--- script/01download data.R
+++ script/01download data.R
@@ -1,10 +1,10 @@
-#### Preamble ####
+
 # Purpose: Get data on 2011-2023 Toronto marriage license and make table
 # Author: Jingyi Shen
 # Email: jingyi.shen@mail.utoronto.ca
 # Date: 22 January 2024
 # Prerequisites: -
-#### Workspace setup ####
+
 install.packages("opendatatoronto")
 install.packages("knitr")
 
@@ -18,7 +18,7 @@ marriage_resources <- marriage_packages |>
   list_package_resources()
 
 Toronto_marriage<-
-  # "https://open.toronto.ca/dataset/marriage-licence-statistics/"
+   "https://open.toronto.ca/dataset/marriage-licence-statistics/"
   list_package_resources("e28bc818-43d5-43f7-b5d9-bdfb4eda5feb") |>
   filter(name == "Marriage Licence Statistics Data.csv") |>
   get_resource()
